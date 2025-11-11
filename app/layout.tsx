8ba1569130b92059910e827bd66fec5215da5049
Rachit Bhatta <rachitbhatta2009@gmail.com>
2025-11-11 15:29:52 +0545

    Add navigation bar, event listing, and event card components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk,Martian_Mono } from "next/font/google";
 import LightRays from '../components/LightRays';
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const schibsted_Grotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -28,17 +29,19 @@ export default function RootLayout({
       <body
         className={`${schibsted_Grotesk.variable} ${martian_Mono.variable} min-h-screen antialiased`}
       >
+      <NavBar/>
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          
           <LightRays
             raysOrigin="top-center"
             raysColor="#5dfeca"
             raysSpeed={0.9}
             lightSpread={5}
-            rayLength={1.4}
+            rayLength={2}
             followMouse={true}
             mouseInfluence={0.01}
             noiseAmount={0.0}
-            distortion={0.005}
+            distortion={0.05}
             className="custom-rays"
           />
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import Explorebtn from '@/components/Explorebtn'
+import EventCard from '@/components/EventCard'
+import events from '@/lib/constants'
 const page = () => {
   return (
     <section>
+      
       <h1 className='text-center '>
           The Hub for every Dev.<br />Event You Can't Miss
       </h1>
+      <Explorebtn/>
+
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'>
+          {events.map((event)=>(
+            <li key={event.title}>
+              <EventCard {...event}/>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
